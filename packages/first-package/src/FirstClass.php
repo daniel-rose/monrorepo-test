9b045102a7acff1ae69368df72c90b971569c474
Daniel Rose <daniel-rose@gmx.de>
2019-07-08 07:28:35 +0200

    boom

--- packages/first-package/src/FirstClass.php
+++ packages/first-package/src/FirstClass.php
@@ -4,4 +4,27 @@ namespace YourMonorepo\FirstPackage;
 
 final class FirstClass
 {
+    /**
+     * @var null|string
+     */
+    protected $firstAttribute;
+
+    /**
+     * @return string|null
+     */
+    public function getFirstAttribute(): ?string
+    {
+        return $this->firstAttribute;
+    }
+
+    /**
+     * @param string|null $firstAttribute
+     * @return FirstClass
+     */
+    public function setFirstAttribute(?string $firstAttribute): FirstClass
+    {
+        $this->firstAttribute = $firstAttribute;
+
+        return $this;
+    }
 }
